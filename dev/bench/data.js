a2ffb9be0bed3e7b5c9f1c6518d0f2419aae0555
github-action-benchmark <github@users.noreply.github.com>
2024-03-27 15:56:06 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 4365f3b59c7135015f74b40fe1605db0599d4363

---
+++ dev/bench/data.js
@@ -0,0 +1,80 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1711554966364,
+  "repoUrl": "https://github.com/xdg-net/Xdg.Directories",
+  "entries": {
+    "Benchmark.Net Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "sam@samtherapy.net",
+            "name": "Sam Therapy",
+            "username": "SamTherapy"
+          },
+          "committer": {
+            "email": "sam@samtherapy.net",
+            "name": "Sam Therapy",
+            "username": "SamTherapy"
+          },
+          "distinct": true,
+          "id": "4365f3b59c7135015f74b40fe1605db0599d4363",
+          "message": "readme: Add docs link as badge\n\nSigned-off-by: Sam Therapy <sam@samtherapy.net>",
+          "timestamp": "2024-03-27T16:09:32+01:00",
+          "tree_id": "62336de7b36de877109f8eb484fa93f77c79d2a1",
+          "url": "https://github.com/xdg-net/Xdg.Directories/commit/4365f3b59c7135015f74b40fe1605db0599d4363"
+        },
+        "date": 1711554920703,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Program+Benchmarks.DataHome",
+            "value": 175.7284505026681,
+            "unit": "ns",
+            "range": "± 0.5529384358484588"
+          },
+          {
+            "name": "Program+Benchmarks.DataDirs",
+            "value": 272.17428867633527,
+            "unit": "ns",
+            "range": "± 0.5683360140312761"
+          },
+          {
+            "name": "Program+Benchmarks.DesktopDir",
+            "value": 769.2368825276693,
+            "unit": "ns",
+            "range": "± 2.6102402291985207"
+          },
+          {
+            "name": "Program+Benchmarks.Home",
+            "value": 86.20164224079677,
+            "unit": "ns",
+            "range": "± 0.1243654370241275"
+          },
+          {
+            "name": "Program+Benchmarks.DataHome",
+            "value": 628.4814425877163,
+            "unit": "ns",
+            "range": "± 1.1000883557497318"
+          },
+          {
+            "name": "Program+Benchmarks.DataDirs",
+            "value": 939.2628009502704,
+            "unit": "ns",
+            "range": "± 1.9793648005890288"
+          },
+          {
+            "name": "Program+Benchmarks.DesktopDir",
+            "value": 2862.156895228795,
+            "unit": "ns",
+            "range": "± 5.33086681328353"
+          },
+          {
+            "name": "Program+Benchmarks.Home",
+            "value": 305.52198546273365,
+            "unit": "ns",
+            "range": "± 1.192952644587444"
+          }
+        ]
+      }
+    ]
+  }
+}
