505646a6ab0c7cdacc604fa28fb8e984e6e08a59
github-action-benchmark <github@users.noreply.github.com>
2024-04-10 13:36:29 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for a6190d44877116fe3f55e5d6977623b37a58f4c2

---
+++ dev/bench/data.js
@@ -0,0 +1,80 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1712756189069,
+  "repoUrl": "https://github.com/xdg-net/Xdg.Directories",
+  "entries": {
+    "Benchmark.Net Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "sam@samtherapy.net",
+            "name": "Sam Therapy",
+            "username": "SamTherapy"
+          },
+          "distinct": true,
+          "id": "a6190d44877116fe3f55e5d6977623b37a58f4c2",
+          "message": "build(deps): Bump peaceiris/actions-gh-pages from 3 to 4\n\nBumps [peaceiris/actions-gh-pages](https://github.com/peaceiris/actions-gh-pages) from 3 to 4.\n- [Release notes](https://github.com/peaceiris/actions-gh-pages/releases)\n- [Changelog](https://github.com/peaceiris/actions-gh-pages/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/peaceiris/actions-gh-pages/compare/v3...v4)\n\n---\nupdated-dependencies:\n- dependency-name: peaceiris/actions-gh-pages\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2024-04-10T15:32:10+02:00",
+          "tree_id": "5672e1d4c1edddf8e9afe5de3468e3bbf322ad77",
+          "url": "https://github.com/xdg-net/Xdg.Directories/commit/a6190d44877116fe3f55e5d6977623b37a58f4c2"
+        },
+        "date": 1712756178621,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Program+Benchmarks.DataHome",
+            "value": 178.52004845937094,
+            "unit": "ns",
+            "range": "± 0.8027195992248912"
+          },
+          {
+            "name": "Program+Benchmarks.DataDirs",
+            "value": 283.008388372568,
+            "unit": "ns",
+            "range": "± 0.37490681780845214"
+          },
+          {
+            "name": "Program+Benchmarks.DesktopDir",
+            "value": 768.7603746141706,
+            "unit": "ns",
+            "range": "± 2.3771114242847924"
+          },
+          {
+            "name": "Program+Benchmarks.Home",
+            "value": 87.78526186943054,
+            "unit": "ns",
+            "range": "± 0.24952072760491353"
+          },
+          {
+            "name": "Program+Benchmarks.DataHome",
+            "value": 626.4163085392544,
+            "unit": "ns",
+            "range": "± 3.867733985901048"
+          },
+          {
+            "name": "Program+Benchmarks.DataDirs",
+            "value": 965.5018261500767,
+            "unit": "ns",
+            "range": "± 5.404622880511332"
+          },
+          {
+            "name": "Program+Benchmarks.DesktopDir",
+            "value": 2852.4108378092446,
+            "unit": "ns",
+            "range": "± 13.372765712262298"
+          },
+          {
+            "name": "Program+Benchmarks.Home",
+            "value": 304.22479311625165,
+            "unit": "ns",
+            "range": "± 1.227818025504474"
+          }
+        ]
+      }
+    ]
+  }
+}
