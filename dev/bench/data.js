e3e6fbec7e539749096ee3c44d3f8508ca3181b8
github-action-benchmark <github@users.noreply.github.com>
2024-07-10 19:42:44 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 221ffd300784e5d6b05cac29b55a07cafd9f3598

---
+++ dev/bench/data.js
@@ -0,0 +1,80 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1720640564034,
+  "repoUrl": "https://github.com/xdg-net/Xdg.Directories",
+  "entries": {
+    "Benchmark.Net Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "sam@samtherapy.net",
+            "name": "Sam Therapy",
+            "username": "SamTherapy"
+          },
+          "distinct": true,
+          "id": "221ffd300784e5d6b05cac29b55a07cafd9f3598",
+          "message": "build(deps): Bump MSTest.TestFramework from 3.3.1 to 3.4.3\n\nBumps [MSTest.TestFramework](https://github.com/microsoft/testfx) from 3.3.1 to 3.4.3.\n- [Release notes](https://github.com/microsoft/testfx/releases)\n- [Changelog](https://github.com/microsoft/testfx/blob/main/docs/Changelog.md)\n- [Commits](https://github.com/microsoft/testfx/compare/v3.3.1...v3.4.3)\n\n---\nupdated-dependencies:\n- dependency-name: MSTest.TestFramework\n  dependency-type: direct:production\n  update-type: version-update:semver-minor\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2024-07-10T19:38:43Z",
+          "tree_id": "461261f9ed3b03823258611cd3adf34be19485a5",
+          "url": "https://github.com/xdg-net/Xdg.Directories/commit/221ffd300784e5d6b05cac29b55a07cafd9f3598"
+        },
+        "date": 1720640558019,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Program+Benchmarks.DataHome",
+            "value": 178.93958886464438,
+            "unit": "ns",
+            "range": "± 0.4360373567304985"
+          },
+          {
+            "name": "Program+Benchmarks.DataDirs",
+            "value": 280.2542209625244,
+            "unit": "ns",
+            "range": "± 0.8448185578412167"
+          },
+          {
+            "name": "Program+Benchmarks.DesktopDir",
+            "value": 745.8337306976318,
+            "unit": "ns",
+            "range": "± 1.3973230765515197"
+          },
+          {
+            "name": "Program+Benchmarks.Home",
+            "value": 86.66456937789917,
+            "unit": "ns",
+            "range": "± 0.2234866358133085"
+          },
+          {
+            "name": "Program+Benchmarks.DataHome",
+            "value": 618.3884107149564,
+            "unit": "ns",
+            "range": "± 1.068183162078533"
+          },
+          {
+            "name": "Program+Benchmarks.DataDirs",
+            "value": 916.116544178554,
+            "unit": "ns",
+            "range": "± 7.65743424283359"
+          },
+          {
+            "name": "Program+Benchmarks.DesktopDir",
+            "value": 2723.96733210637,
+            "unit": "ns",
+            "range": "± 25.479934917979545"
+          },
+          {
+            "name": "Program+Benchmarks.Home",
+            "value": 310.68222182137623,
+            "unit": "ns",
+            "range": "± 0.9788624167024504"
+          }
+        ]
+      }
+    ]
+  }
+}
